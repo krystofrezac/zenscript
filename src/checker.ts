bd565dd9ecc97840b754768c8edfb89733fc6ea5
Kryštof Řezáč <krystofrezac@gmail.com>
2023-01-21 15:35:14 +0100

    Working on type ast

---
+++ src/checker.ts
@@ -0,0 +1,129 @@
+import { MatchResult } from "ohm-js";
+import grammar from "./grammar.ohm-bundle";
+
+type Type = {
+    type: "string" | "number";
+  } 
+  | {
+    type: "referenceToVariable",
+    variableName: string
+  }  
+  | {
+    type: "block",
+    content: StatementItem | StatementItem[]
+  }  
+  | {
+    type: "functionCall",
+    typeOfCalledExpression: Type,
+    arguments: Type[],
+  }
+  | {
+    type: "function",
+    parameters: Type[],
+  }
+
+type StatementItem = {
+  name: string,
+  statementType: Type
+} | {
+  statementType: Type
+}
+
+declare module 'ohm-js' {
+  interface Node {
+    getType: () => Type|Type[];
+    getStatementItemInfo: () => StatementItem|StatementItem[]
+  }
+}
+
+const semantics = grammar.createSemantics()
+
+const toArray = <T>(item: T | T[]) => Array.isArray(item) ? item : [item]
+
+const createType = (type: Type)=>type
+const createStatementItem = (item: StatementItem) => item
+
+semantics.addOperation<Type|Type[]>("getType", {
+  _iter(...children) {
+    return children.flatMap(c=>c.getType())
+  },
+  _terminal(){
+    return []
+  },
+  string(_startQuotes, _string, _endQuotes) {
+    return createType({
+      type: "string",
+    })
+  },
+  number(_number){
+    return createType({
+      type: "number"
+    })
+  },
+  identifier(name) {
+    return createType({type: "referenceToVariable", variableName: name.sourceString}) 
+  },
+  Block(_startCurly, _emptyLines, blockStatement, _endCurly) {
+    return createType({ type: "block", content: blockStatement.getStatementItemInfo()})
+  },
+  FunctionCall_firstCall(identifier, _startBrace, passedArguments, _endBrace) {
+    return createType({
+      type: "functionCall", 
+      typeOfCalledExpression: identifier.getType() as Type, 
+      arguments: toArray(passedArguments.getType())
+    }) 
+  },
+  FunctionCall_chainedCall(previousCall, _startBrace, passedArgument, _endBrace) {
+    return createType({
+      type: "functionCall", 
+      typeOfCalledExpression: previousCall.getType() as Type, 
+      arguments: toArray(passedArgument.getType())
+    })
+  },
+  FunctionArguments_arguments(argument, _comma, resetArguments) {
+    return [argument.getType(), resetArguments.getType()].flat()
+  },
+})
+
+semantics.addOperation<StatementItem| StatementItem[]>("getStatementItemInfo", {
+  _iter(...children) {
+    return children.flatMap(c => {
+      return c.getStatementItemInfo()
+    })
+  },
+  _terminal(){
+    return []
+  },
+  Program_statements(firstStatement, _emptyLines, secondStatement) {
+    const res = [firstStatement.getStatementItemInfo(), secondStatement.getStatementItemInfo()]
+    return res.flat();
+  },
+  Program_wrappedInEmptyLines(_startEmptyLInes, statement, _endEmptyLines) {
+    return statement.getStatementItemInfo()
+  },
+  VariableDeclaration(identifier, _equals, expression) {
+    return createStatementItem({
+      name: identifier.sourceString,
+      statementType: expression.getType() as Type
+    })
+  },
+  Expression(expression) {
+    return createStatementItem({statementType: expression.getType() as Type}) 
+  },
+  BlockStatement_statements(statement, _emptyLines, blockStatement) {
+    return [statement.getStatementItemInfo(), blockStatement.getStatementItemInfo()].flat()
+  },
+  BlockStatement_endStatement(expression, _emptyLines) {
+    return expression.getStatementItemInfo()
+  },
+})
+
+export const check = (parsedInput: MatchResult) => {
+  if(parsedInput.succeeded()){
+    const adapter = semantics(parsedInput)
+    return adapter.getStatementItemInfo()
+  } else {
+    console.error("Failed to parse", parsedInput.message);
+    return undefined;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
+import { check } from './checker'
 import { parse } from './parser'
-import { transpile } from './transpiler'
 
 const code = `
-  hof = (a)(b){
-    add = jsExternal!("(j,k)=>j+k")
-    add(a, b)
-  }
-  a = hof(1)(2)
+  c = 1
+  b = a(1,"2")()
 `
 
 const parsed = parse(code)
-console.log(transpile(parsed))
+console.log(JSON.stringify(check(parsed), undefined, 2))
